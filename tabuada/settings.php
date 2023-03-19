8d284dc50c0a54fdb1215f048c15fa8182accff9
Thallys <antoniothallys2017@gmail.com>
2023-03-19 12:12:37 -0700

    implementação de função para melhorar o codigo

--- tabuada/settings.php
+++ tabuada/settings.php
@@ -12,14 +12,35 @@
             $num = $_GET["num"];
             $oper = strtoupper($_GET["oper"]);
 
+            function calc($nume, $opera){
+                if($opera == "SUM"){
+                    $opera = "+";
+                } elseif($opera == "MULTI"){
+                    $opera = "*";
+                } elseif($opera == "SUB"){
+                    $opera = "-";
+                } else{
+                    $opera = "/";
+                }
+
+                for($i=1;$i<=10;$i++){
+                    $calc = $nume . $opera . $i;
+                    echo "<p>A tabuada de $nume é: <br>
+                            $nume $opera $i = $calc <br>
+                    </p>";
+                }
+
+            }
             switch($oper){
                 case "MULTI":
-                    for($i=1;$i<=10;$i++){
-                        $calc = $num * $i;
-                        echo "<p>A tabuada de $num é: <br>
-                                $num x $i = $calc <br>
-                        </p>";
-                    } 
+                    /** 
+                    *for($i=1;$i<=10;$i++){
+                    *    $calc = $num * $i;
+                    *  echo "<p>A tabuada de $num é: <br>
+                    *           $num x $i = $calc <br>
+                    *    </p>";
+                    *} */
+                    calc($num, $oper);
                     break;
                 case "SUB":
                     for($i=1;$i<=10;$i++){
