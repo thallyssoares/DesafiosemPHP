ec7b3c3b062356b47da10a8fb2e41d28df5a4960
Thallys <antoniothallys2017@gmail.com>
2023-03-25 06:51:47 -0700

    implementação de função pra calcular descontos no salario do trabalhador

--- hora-trabalho/settings.php
+++ hora-trabalho/settings.php
@@ -9,23 +9,49 @@
 <body>
     <main>
         <?php 
-        
-            function hrTrabalhada(){
-                $hr = $_GET["hr"];
-                $sal = $_GET["sal"];
+            
+            $hr = $_GET["hr"];
+            $sal = $_GET["sal"];
 
-                $hrMes = $hr * 30;
-                $vrHr = number_format(($sal / $hrMes), 2);
+            function hrTrabalhada($hora, $sala){
+                
+                //função pra calculo da hr trabalhada
 
-                $salPessoa = ["Hora por Dia" =>$hr,
-                "Salario Mensal" => $sal, "Hora total do mês" => $hrMes, "Valor da hora"=>$vrHr];
+                $hrMes = $hora * 30;
+                $vrHr = number_format(($sala / $hrMes), 2);
+
+                $salPessoa = ["Hora por Dia" =>$hora,
+                "Salario Mensal" => $sala, "Hora total do mês" => $hrMes, "Valor da hora"=>$vrHr];
                 
                 foreach ($salPessoa as $indice => $valor) {
                     echo "$indice: $valor. <br>";
                 }
+
             }
-            hrTrabalhada();
-        
+            function descontosSalarios($salario){
+                //função pra calcular os descontos no seu salario.
+                //IR = 11%, INSS = 8%, Sindicato = 5%.
+
+                $descInss = ($salario /100)*8;
+                $descIR = ($salario/100)*11;
+                $descSindi = ($salario/100)*5;
+
+                echo "<hr>";
+                if($salario >= 1903){
+
+                
+                    $salLiquido = $salario - $descInss - $descIR - $descSindi;
+
+                    echo "+ Salario Bruto: $salario <br> - INSS (8%): $descInss <br> - IR (11%): $descIR <br> - Sindicato: $descSindi <br> = Salario Liquido: $salLiquido";
+
+                }else{
+                    $salLiquido = $salario - $descInss - $descSindi;
+                    echo "+ Salario Bruto: $salario <br> - INSS (8%): $descInss <br> - Sindicato: $descSindi <br> = Salario Liquido: $salLiquido <br>";
+                }
+            }
+            
+            hrTrabalhada($hr, $sal);
+            descontosSalarios($sal);
         ?>
         <a href="index.html">Voltar</a>
     </main>
